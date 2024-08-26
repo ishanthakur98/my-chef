510653c9229209d743523e2c7eebc553d5e8820f
LP015574\ishan.thakur_spicemo <ishan.thakur@spicemoney.com>
2024-08-26 19:50:51 +0530

    Initial commit

---
+++ src/main/java/com/my_chef/MyChefApplication.java
@@ -0,0 +1,13 @@
+package com.my_chef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyChefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyChefApplication.class, args);
+	}
+
+}
